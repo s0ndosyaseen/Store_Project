fdd757faa01cd7c6c62ff140393d57b90cceea5b
sondos <sondos.yaseen85@gmail.com>
2026-05-11 23:57:40 +0300

    بحس كان قصدك هون لما حكيتي انه يرجع لاخر صفحة كان داخلها لانه بالديتيلز محل ما ضفتيها هناك دائما بتكون اخر صفجة فتحها هي الرئيسية ع اي حال انا حاسة انه هاد الزر مالوش اي ستين لازمة وخلينا نحذفه

--- admin/products_manager.php
+++ admin/products_manager.php
@@ -90,7 +90,9 @@ $products = $pdo->query("SELECT * FROM products ORDER BY id DESC")->fetchAll();
      <button class="open-btn" onclick="openNav()" style="margin-left: 30px;">
         <i class="fas fa-bars"></i>
     </button>
-    <a href="index.php" style="color: #c4a35a;"><i class="fas fa-home"></i> العودة للرئيسية</a>
+     <a href="javascript:history.back()" style="color: #c4a35a;" >
+        <i class="fas fa-arrow-right"></i> عودة
+    </a>
   </div>
     <h1><i class="fas fa-box"></i> إدارة المنتجات</h1>
     <a href="add_product.php" class="btn-add"><i class="fas fa-plus"></i> إضافة منتج جديد</a>
