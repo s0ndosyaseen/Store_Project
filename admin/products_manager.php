1c83a55bf18913bd3d46b56422918e215914121a
layan <layannid2112@gmail.com>
2026-05-12 21:51:27 +0300

    حذف الكبسة

--- admin/products_manager.php
+++ admin/products_manager.php
@@ -90,10 +90,7 @@ $products = $pdo->query("SELECT * FROM products ORDER BY id DESC")->fetchAll();
      <button class="open-btn" onclick="openNav()" style="margin-left: 30px;">
         <i class="fas fa-bars"></i>
     </button>
-     <a href="javascript:history.back()" style="color: #c4a35a;" >
-        <i class="fas fa-arrow-right"></i> عودة
-    </a>
-  </div>
+       </div>
     <h1><i class="fas fa-box"></i> إدارة المنتجات</h1>
     <a href="add_product.php" class="btn-add"><i class="fas fa-plus"></i> إضافة منتج جديد</a>
 </header>
