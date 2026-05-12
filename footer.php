362321e098c37fdba1e9a225cb0726d84c9cb093
sondos <sondos.yaseen85@gmail.com>
2026-05-12 17:14:12 +0300

    هلا هون كنت بس بدي احط الفوتر واالهيدر بي اتش بي وغيرت الفكرة بعدها بس عادي يعني استعملته للفوتر بالكتاجوري لكن ممكن ينلغى لانه ما استعملته غير هناك ما حبيت اغير الهوم لبي اتش بي عشان بدي اغير امتداده بباقي الصفحات لهيك عدلت الفوتر يدويا

---
+++ footer.php
@@ -0,0 +1,81 @@
+ <div class="footer">
+        <div class="horz">
+            <div class="feature-item">
+            <div class="icon-wrapper">
+                <i class="fa-solid fa-truck-fast"></i>
+            </div>
+            <h3>شحن دولي فاخر</h3>
+            <p>نصل إليك أينما كنت بتغليف يحمي القطع ويليق بقيمتها الفنية والتاريخية.</p>
+         </div>
+
+         <div class="feature-item">
+            <div class="icon-wrapper">
+                <i class="fa-solid fa-shield-check"></i> <i class="fa-solid fa-certificate"></i>
+            </div>
+            <h3>ضمان الأصالة</h3>
+            <p>كل قطعة تأتي مع شهادة توثيق توضح المصدر الثقافي والمواد المستخدمة في صناعتها.</p>
+         </div>
+
+         <div class="feature-item">
+            <div class="icon-wrapper">
+                <i class="fa-solid fa-rotate-left"></i>
+            </div>
+            <h3>استبدال سهل</h3>
+            <p>رضاكم هو أولويتنا، نوفر سياسة استبدال مرنة تضمن حصولكم على ما تطمحون إليه.</p>
+         </div>
+
+        </div>
+        <!-- footer  -->
+        <div class="horz">
+            <div class="footer-column newsletter">
+                <h4>هل لديكِ حساب؟</h4>
+                <div class="subscribe-form">
+                    <input type="email" id="quick-email" placeholder="أدخلي بريدك الإلكتروني">
+                    <button type="button" onclick="goToLogin()">تسجيل دخول</button>
+                </div>
+                <p>سجلي دخولك الآن لتتمكني من إتمام عملية الشراء ومتابعة طلباتك السابقة.</p>
+            </div>
+
+            <div class="footer-column">
+                <h4>التسوق</h4>
+                <ul>
+                    <li><a href="category.php?type=andalus">الحضارة الأندلسية</a></li>
+                    <li><a href="category.php?type=egypt">الحضارة الفرعونية</a></li>
+                    <li><a href="category.php?type=sham">الحضارة الشامية</a></li>
+                    <li><a href="category.php?type=victory">الفيكتورية</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-column">
+                <h4>عن البوصلة</h4>
+                <ul>
+                    <li><a href="home.html#story">قصتـنا</a></li>
+                    <li><a href="admin/index.php">تسجيل الدخول للادارة والموظفين</a></li>
+                    <li><a href="contact.php">تواصل معنا</a></li>
+                    <li><a href="cart-full.html" class="login-btn">سلة المشتريات</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-logo">
+                <img src="images/logo.png" alt="البوصلة - موقع التراث والحضارات">
+            </div>
+        </div>
+
+        <div class="footer-bottom">
+            <div class="copyright">
+                <p>&copy; جميع الحقوق محفوظة لـ (Al-Bousala) البوصلة 2026</p>
+            </div>
+            
+            <div class="social-icons">
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+            </div>
+
+            <div class="bottom-links">
+                <a href="#">الشروط والأحكام</a>
+                <a href="#">سياسة الخصوصية</a>
+            </div>
+        </div>
+
+     </div>
